8107b39cd3e4bc6b5d7d2470cb068cad1e2a6baa
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-11 16:36:58 +0000

    made heads or tails game

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 #Write your code below this line 👇
 #Hint: Remember to import the random module first. 🎲
+import random
+print(" Welcome to my Heads or tails game")
+choice = input(" call it: heads or tails ?:" )
+coin = random.randint(0,1)
+
+if coin ==0 :
+ print("it's heads ")
+else:
+  print("it;s tails") 
+  
+
+
+
+
 
 
 
